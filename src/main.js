56362838fb655d24ef2fe308c8f0a752a1c67417
santinakc <santinakcfin@gmail.com>
2023-04-09 16:57:32 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/* Menu mobile */
+
+const menu = document.getElementById("menu");
+const menuBtn = document.getElementById("menu_btn");
+const body = document.getElementsByTagName("body")[0];
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+  /* body.classList.toggle("hidden"); */
+});
